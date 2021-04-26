d911b9236546fc429a67ad4d9c75673bce98ca61
ArimaBatista <80778627+ArimaBatista@users.noreply.github.com>
2021-04-26 17:30:21 -0300

    Add files via upload

--- Lab 05/JogoDamas/JogoDamas/src/Jogo/Damas.java
+++ Lab 05/JogoDamas/JogoDamas/src/Jogo/Damas.java
@@ -6,13 +6,5 @@ public class Damas
 	char j1 = 'B';
 	char j2 = 'P';
 	Tabuleiro tt;
-	void liga(Tabuleiro tt) 
-	{
-		this.tt = tt;
-		for (int b = 1; b < 9; b++)
-		{
-			tt.tabua[4][b] = j1;
-			tt.tabua[3][b] = j2;
-		}
-	}
+	void liga(Tabuleiro tt) { this.tt = tt; }
 }
--- Lab 05/JogoDamas/JogoDamas/src/Jogo/Tabuleiro.java
+++ Lab 05/JogoDamas/JogoDamas/src/Jogo/Tabuleiro.java
@@ -3,6 +3,9 @@ package Jogo;
 public class Tabuleiro 
 {
 	char tabua[][] = new char[9][9];
+	Damas dama;
+	Pecas peca;
+	String comando;
 	Tabuleiro()
 	{
 		for (int i = 0; i < 9; i++)
@@ -12,11 +15,35 @@ public class Tabuleiro
 				tabua[i][b] = '-';
 			}
 		}
-		for (int i = 0; i < 8; i++) { tabua[i][0] = (char) ('1' + i);}
+		for (int i = 0; i < 8; i++) { tabua[i][0] = (char) ('8' - i);}
 		for (int i = 0; i < 8; i++) { tabua[8][i+1] = (char) ('a' + i);}
 		tabua[8][0] = ' ';
 	}
+	
+	void calldama(Damas dama) { this.dama=dama; }
+	void callpeca(Pecas peca) { this.peca=peca; }
 
+	void controle(String comando)
+	{
+		this.comando = comando;
+		int c[] = new int[4];
+		char chars[] = comando.toCharArray();
+		for (int g = 0; g < 4; g++)
+		{
+			c[g] = 0;
+		}
+		System.out.print(comando + "\n");		
+		c[0] = chars[0] - 'a';
+		c[1] = chars[1] - '1';
+		c[2] = chars[3] - 'a';
+		c[3] = chars[4] - '1';
+		for (int i = 0; i < 4; i++)
+		{
+			System.out.print(c[i]);		
+		}
+		System.out.print("\n");	
+	}
+	
 	void mostra()
 	{
 		System.out.print("\n");
